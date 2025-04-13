10670d9eb2e8cfee8a080ee58eecb7f109f315f0
Vivek <vivekousik8555@gmail.com>
2025-04-13 15:40:10 +0530

    Done with creating the folder structure and the auth screens and UI

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+const Rootlayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className='root-layout'>
+            {children}
+        </div>
+    )
+}
+
+export default Rootlayout
